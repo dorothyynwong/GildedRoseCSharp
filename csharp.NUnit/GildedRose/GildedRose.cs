373cf955540841310aa5f70ac2aa651417b18482
dorothywyn <dorothywyn@users.noreply.github.com>
2024-08-06 17:54:41 +0100

    refractoring GildedRose.cs, completed first If

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -15,47 +15,65 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
+            switch (Items[i].Name) {
+                case "Aged Brie":
+                    if (Items[i].Quality < 50) Items[i].Quality = Items[i].Quality + 1;
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (Items[i].Quality < 50) Items[i].Quality = Items[i].Quality + 1;
+                    if (Items[i].SellIn < 11 && Items[i].Quality < 50) Items[i].Quality = Items[i].Quality + 1;
+                    if (Items[i].SellIn < 6 && Items[i].Quality < 50) Items[i].Quality = Items[i].Quality + 1;
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    if (Items[i].Quality > 0) Items[i].Quality--;
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                    break;
             }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
+            // if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            // {
+                // if (Items[i].Quality > 0)
+                // {
+                //     if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                //     {
+                //         // Items[i].Quality = Items[i].Quality - 1;
+                //     }
+                // }
+            // }
+            // else
+            // {
+                // if (Items[i].Quality < 50)
+                // {
+                //     // Items[i].Quality = Items[i].Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                //     if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                //     {
+                //         if (Items[i].SellIn < 11)
+                //         {
+                //             if (Items[i].Quality < 50)
+                //             {
+                //                 // Items[i].Quality = Items[i].Quality + 1;
+                //             }
+                //         }
 
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+                //         if (Items[i].SellIn < 6)
+                //         {
+                //             if (Items[i].Quality < 50)
+                //             {
+                //                 // Items[i].Quality = Items[i].Quality + 1;
+                //             }
+                //         }
+                //     }
+                // }
+            // }
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
+            // if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            // {
+            //     Items[i].SellIn = Items[i].SellIn - 1;
+            // }
 
             if (Items[i].SellIn < 0)
             {
